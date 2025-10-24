d0c60beb3246c1b05654e6326e8938f6cf3e5c30
brianmugunga <mugungabrian10@gmail.com>
2025-10-24 14:42:15 +0200

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/kwanbh-website/',
+  base: '/kwanwebsite_final/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
